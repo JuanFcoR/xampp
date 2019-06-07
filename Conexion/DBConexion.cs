207feee6df6e0d1768a2e8207c88f34bbe6ac8b0
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2019-06-06 23:10:42 -0400

    Agregando una segunda coneccion

--- Conexion/DBConexion.cs
+++ Conexion/DBConexion.cs
@@ -11,7 +11,10 @@ namespace XamppTest.Conexion
     {
         public static MySqlConnection ObtenerConexion()
         {
-            string strconn = "server =192.168.8.21; database = mibase; Uid=root; Port=3306; pwd=proyecto;";
+            string strconn = "server=bsnapgkczbf2ejnqiqwf-mysql.services.clever-cloud.com; " +
+                "database = bsnapgkczbf2ejnqiqwf; Uid=ua2uw7zhs4lowjyz; Port=3306; pwd=CLJ3IanKEAlO21fgexVZ;";
+
+            //string strconn2 = "server =192.168.137.1; database = mibase; Uid=root; Port=3306; pwd=proyecto;";
             MySqlConnection conn = new MySqlConnection(strconn);
             conn.Open();
             return conn;
